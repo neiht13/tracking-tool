34b646943910f4bb666f834223c9769020fb596a
Trung Hoang <hvtrung@tma.com.vn>
2018-04-02 20:05:05 +0700

    init

---
+++ src/main/java/com/tma/tt/api/example/IArticleService.java
@@ -0,0 +1,11 @@
+package com.tma.tt.api.example;
+
+import java.util.List;
+
+public interface IArticleService {
+     List<Article> getAllArticles();
+     Article getArticleById(int articleId);
+     boolean addArticle(Article article);
+     void updateArticle(Article article);
+     void deleteArticle(int articleId);
+}

ad94796d1e4c3600028c1ff5c590c8e726e76cf9
neiht13 <thienphan9612@gmail.com>
2018-07-01 02:34:36 +0700

    qqwe

--- src/main/java/com/tma/tt/api/config/SecurityConfig.java
+++ src/main/java/com/tma/tt/api/config/SecurityConfig.java
@@ -84,8 +84,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers("/api/auth/**")
                         .permitAll()
                     .anyRequest()
-                        .authenticated();
-
+                        .permitAll();
         // Add our custom JWT security filter
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
